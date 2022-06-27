4260f7826a296017bab6dea097bcc4d2f43c997b
Atos Neves <atos.neves100@gmail.com>
2022-06-27 00:37:37 -0300

    Continuação portfolio

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
 
-let h = window.innerHeight;
-console.log(h);
-hh = h +"px";
-
-
-let hei = document.getElementById("entrada-principal");
-hei.style.height= hh;
-
-// let heiSobre = h-400;
-// heiSobre = heiSobre+"px";
-// let heiSobreDiv = document.querySelector(".sobre-mim");
-// heiSobreDiv.style.height=heiSobre;
-//
-//
-
-
 
 
