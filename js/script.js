1b931b27aa7340385241464df317380f2dde9f54
Atos Neves <atos.neves100@gmail.com>
2022-06-29 09:09:04 -0300

    Continuação portfolio

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@ window.addEventListener("scroll",function (event) {
 	if (scrollTop){
 		var barraPreta = document.querySelector(".barra-preta");
 		var menuLista = document.querySelector(".nav-bar");
+		var teste1 = document.getElementById("container-sobre-mim");
+
+
+		var sobre1 = document.getElementById("caixa-principal-sobre");
+
+		var sobre2 = document.getElementById("caixa-sobre2");
+
+		// sobre2.innerHTML="asdad";
+		var sobre3 = document.getElementById("caixa-sobre3");
 
 		barraPreta.classList.add("barra-preta-comprimida");
 		menuLista.classList.add("menu-comprimido");
@@ -13,15 +22,7 @@ window.addEventListener("scroll",function (event) {
 			barraPreta.classList.replace("barra-preta-comprimida","barra-preta-extendida");
 			menuLista.classList.replace("menu-comprimido","menu-extendido");
 				if (scrollTop>200) {
-					var teste1 = document.getElementById("container-sobre-mim");
 
-
-					var sobre1 = document.getElementById("caixa-principal-sobre");
-
-					var sobre2 = document.getElementById("caixa-sobre2");
-
-					// sobre2.innerHTML="asdad";
-					var sobre3 = document.getElementById("caixa-sobre3");
 
 						sobre1.classList.add("aparecendo2");
 						sobre2.classList.add("aparecendo3")
@@ -30,14 +31,23 @@ window.addEventListener("scroll",function (event) {
 
 
 
-					teste1.classList.add("aparecendo");
+						teste1.classList.add("aparecendo");
 				}
 			}
 		    if (scrollTop<250) {
 			    barraPreta.classList.replace("barra-preta-extendida","barra-preta-comprimida");
 			    menuLista.classList.replace("menu-extendido","menu-comprimido");
 			}
+		if (scrollTop<100) {
+			sobre1.classList.remove("aparecendo2");
+			sobre2.classList.remove("aparecendo3")
+			sobre3.classList.remove("aparecendo4");
 
+
+
+
+			teste1.classList.add("aparecendo");
+		}
 	}
 
 	// var teste = document.querySelectorAll(".entrada-nomes");
@@ -67,3 +77,4 @@ window.addEventListener("scroll",function (event) {
 
 	console.log(scrollTop);
 });
+
